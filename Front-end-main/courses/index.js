9845cbaf2365c3984c8c0440b658e13f08c4ef1c
Soham Sarkar <sohamsarkar59@gmail.com>
2023-03-04 14:34:11 +0530

    design changes in the button

--- Front-end-main/courses/index.js
+++ Front-end-main/courses/index.js
@@ -1,21 +1,20 @@
-const navbar = document.querySelector('.navbar');
-const openmenu = document.querySelector('.openmenu');
-const closemenu = document.querySelector('#closemenu');
+// const navbar = document.querySelector('.navbar');
+// const openmenu = document.querySelector('.openmenu');
+// const closemenu = document.querySelector('#closemenu');
 
+// openmenu.addEventListener('click',show);
+// closemenu.addEventListener('click',close);
 
-openmenu.addEventListener('click',show);
-closemenu.addEventListener('click',close);
+// function show(){
+//     navbar.style.display = 'block';
+//     openmenu.style.display='none';
+//     closemenu.style.display='block';
 
-function show(){
-    navbar.style.display = 'block';
-    openmenu.style.display='none';
-    closemenu.style.display='block';
+// }
 
-}
+// function close(){
+//     navbar.style.display='none';
+//     closemenu.style.display='none';
+//     openmenu.style.display='block';
 
-function close(){
-    navbar.style.display='none';
-    closemenu.style.display='none';
-    openmenu.style.display='block';
-
-}
+// }
